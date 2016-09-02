89cba5a631ca13057de0848ebdc733e9c5b935ea
tomgreenfield <tomgreenfield@users.noreply.github.com>
2016-09-02 12:01:11 +0100

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# adapt-assLearning
-An extension to add a list of associated learning to Assessment Results
+# Ass Learning
+
+An extension to add a list of associated learning to Assessment Results.
+
+## Installation
+
+* For questions with associated learning, list their linked IDs:
+```json
+"_assLearning": [
+	"c-05"
+]
+```
+* Add the reference `{{{assLearning}}}` to the text in the Assessment Results component. 
+* Copy the extension folder into the src > extensions directory and run an appropriate Grunt task.
+
+## Usage
+
+* Associated learning is listed for incorrectly answered questions with linked IDs.
+* Override [assLearning.hbs](templates/assLearning.hbs) to display different attributes for the associated learning.
+
+## Attributes
+
+Attribute | Type | Description | Default
+--------- | ---- | ----------- | -------
+`_assLearning` | Array | The IDs associated with the question | `[]`
---
+++ js/adapt-assLearning.js
@@ -0,0 +1,42 @@
+define([
+	"coreJS/adapt",
+	"components/adapt-contrib-assessmentResults/js/adapt-contrib-assessmentResults"
+], function(Adapt, AssessmentResults) {
+
+	function overrideAssessmentResults() {
+		var setFeedback = AssessmentResults.prototype.setFeedback;
+
+		AssessmentResults.prototype.setFeedback = function(feedbackBand) {
+			var state = this.model.get("_state");
+			var ids = [];
+			var data = [];
+
+			state.questionModels.each(function(question) {
+				if (question.get("_isCorrect")) return;
+
+				var assIds = question.get("_assLearning");
+
+				if (!assIds) return;
+
+				for (var i = 0, j = assIds.length; i < j; i++) {
+					var assId = assIds[i];
+
+					if (!_.contains(ids, assId)) ids.push(assId);
+				}
+			});
+
+			for (var i = 0, j = ids.length; i < j; i++) {
+				var model = Adapt.findById(ids[i]);
+
+				if (model) data.push(model.attributes);
+			}
+
+			state.assLearning = Handlebars.templates.assLearning(data);
+
+			setFeedback.call(this, feedbackBand);
+		};
+	}
+
+	Adapt.once("app:dataReady", overrideAssessmentResults);
+
+});
